8b5cf06f782af675227006cd33cd25df1ba2b80f
Eibhlina <ewelina.kompala@gmail.com>
2024-10-19 18:26:06 +0200

    Poprawione

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './components/App.jsx'
+import './styles/Phonebook.css'
+import './styles/AddUserForm.css'
+import './styles/option.css'
+import { Provider } from 'react-redux'
+import { contactStore } from './redux/contaktStore.js'
+
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={contactStore}>
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </Provider>
+
 )
